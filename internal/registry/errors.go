f4def6dfa756680041694d454c1646579d7ad687
Diogo Pinto <diogo.pinto@imaginecurve.com>
2019-11-12 22:49:27 +0000

    Simplify and improve upon registry's service

--- internal/registry/errors.go
+++ internal/registry/errors.go
@@ -1,1 +1,6 @@
 package registry
+
+const (
+	errMissingImage  = "missing image"
+	errMissingFaceID = "missing face id"
+)
--- internal/registry/service.go
+++ internal/registry/service.go
@@ -1,7 +1,10 @@
+//go:generate mockgen -package registry -source=service.go -destination service_mock.go
+
 package registry
 
 import (
 	"context"
+	"github.com/diogox/dom-face-recognizer/internal/face"
 
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
@@ -12,12 +15,14 @@ import (
 
 type PersonService interface {
 	GetPeople(ctx context.Context) ([]person.Person, error)
+	FindPersonByID(ctx context.Context, id uuid.UUID) (person.Person, error)
 	AddPerson(ctx context.Context, info person.Person) error
 	RemovePerson(ctx context.Context, id uuid.UUID) error
 }
 
 type FaceService interface {
 	RecognizeFace(ctx context.Context, imgBytes []byte) (uuid.UUID, error)
+	FindFacesByPersonID(ctx context.Context, personID uuid.UUID) ([]face.Face, error)
 	AddFace(ctx context.Context, imgBytes []byte, personID uuid.UUID) (faceID uuid.UUID, err error)
 	RemoveFace(ctx context.Context, faceID uuid.UUID) error
 }
@@ -30,7 +35,7 @@ type Service struct {
 
 func NewRegistryService(logger logrus.FieldLogger, personService PersonService, faceService FaceService) *Service {
 	return &Service{
-		logger: logger,
+		logger:        logger,
 		personService: personService,
 		faceService:   faceService,
 	}
@@ -52,14 +57,26 @@ func (s *Service) RemovePerson(ctx context.Context, personID uuid.UUID) error {
 		return errors.New("missing person id")
 	}
 
+	faces, err := s.faceService.FindFacesByPersonID(ctx, personID)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get faces with person id: %v", personID)
+	}
+
+	for _, f := range faces {
+		err := s.faceService.RemoveFace(ctx, f.ID)
+		if err != nil {
+			return errors.Wrapf(err, "failed to remove face associated to person. face id: %v", f.ID)
+		}
+	}
+
 	return s.personService.RemovePerson(ctx, personID)
 }
 
 func (s *Service) RecognizeFace(ctx context.Context, imgBytes []byte) (person.Person, error) {
 	// TODO: Use 'validate' for this?
 
-	if len(imgBytes) == 0 {
-		return person.Person{}, errors.New("missing image")
+	if imgBytes == nil || len(imgBytes) == 0 {
+		return person.Person{}, errors.New(errMissingImage)
 	}
 
 	personID, err := s.faceService.RecognizeFace(ctx, imgBytes)
@@ -67,29 +84,20 @@ func (s *Service) RecognizeFace(ctx context.Context, imgBytes []byte) (person.Pe
 		return person.Person{}, err
 	}
 
-	// TODO: IMPORTANT! Create GetPersonByID method in PeopleService so we can get rid of this
-	var recognized *person.Person
-	people, _ := s.GetPeople(ctx)
-	for _, p := range people {
-		if p.ID == personID {
-			recognized = &p
-			break
-		}
-	}
-
-	if recognized == nil {
+	recognized, err := s.personService.FindPersonByID(ctx, personID)
+	if err != nil {
 		s.logger.Debugf("recognized person, but failed to get person by id: %v", personID)
-		return person.Person{}, errors.Errorf("no match was found for person id: %v", personID)
+		return person.Person{}, errors.Wrapf(err, "no match was found for person id: %v", personID)
 	}
 
-	return *recognized, nil
+	return recognized, nil
 }
 
 func (s *Service) AddFace(ctx context.Context, imgBytes []byte, personID uuid.UUID) (uuid.UUID, error) {
 	// TODO: Use 'validate' for this?
 
-	if len(imgBytes) == 0 {
-		return uuid.Nil, errors.New("missing image")
+	if imgBytes == nil || len(imgBytes) == 0 {
+		return uuid.Nil, errors.New(errMissingImage)
 	}
 
 	if personID == uuid.Nil {
@@ -104,7 +112,7 @@ func (s *Service) RemoveFace(ctx context.Context, faceID uuid.UUID) error {
 	// TODO: Use 'validate' for this?
 
 	if faceID == uuid.Nil {
-		return errors.New("missing face id")
+		return errors.New(errMissingFaceID)
 	}
 
 	return s.faceService.RemoveFace(ctx, faceID)
