aff1553a51ec53d7da797d7340f9ef92a8b06e5b
Diogo Pinto <diogo.pinto@imaginecurve.com>
2019-11-16 23:18:21 +0000

    Add RemovePerson client for testing purposes

---
+++ cmd/clients/removePerson/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/diogox/dom-face-registry/pkg/client"
+	pb "github.com/diogox/dom-face-registry/pkg/registry"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const endpoint = ":8080"
+
+func run() error {
+	if len(os.Args) != 2 {
+		return errors.New("need person id as argument")
+	}
+
+	personID := os.Args[1]
+
+	c, conn, err := client.NewClientBuilder().
+		WithTarget(endpoint).
+		Build()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	_, err = c.RemovePerson(context.Background(), &pb.RemovePersonRequest{
+		Id: personID,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully Removed!")
+	return err
+}
